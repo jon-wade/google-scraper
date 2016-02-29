641938ba60b9b8ac5844a3a944f68aaf8636569c
jon-wade <jonwadeuk@gmail.com>
2016-02-29 09:51:59 +0000

    Create routine to extract the href from listings and comment code

--- html-parse.rb
+++ html-parse.rb
@@ -3,9 +3,13 @@ require 'nokogiri'
 
 puts "Input search phrase: "
 input = gets.chomp
-searchTerm = input.split
+searchTerm = input.split #split input at each space delimiter into an array
 
-holdingURL = "https://www.google.co.uk/search?q="
+holdingURL = "https://www.google.co.uk/search?q=" #standard search query
+
+#the following code constructs the search query by appending the search
+#words onto the end of the holding URL and adding the encoding for a space
+#which is %20 to separate each search word in the URL
 
 (0..(searchTerm.length-1)).each {|i|
   holdingURL += searchTerm[i]
@@ -14,21 +18,34 @@ holdingURL = "https://www.google.co.uk/search?q="
   end
 }
 
-puts holdingURL
-
-
-
 baseURL = holdingURL
 
+#use the Nokogiri library to parse the HTML page at the baseURL location
+#and output the search page to the console
 doc = Nokogiri::HTML(open(baseURL))
 puts ""
 puts "SEARCH PAGE: " + baseURL
 
+#search through the HTML page and locate all the tags that contain the
+#search links using the css selector below
 serps = doc.css('h3.r a:not([class!=""])')
-# serpsSuppress = doc.css('h3.r a[class="sla"]')
-# serps = serps-serpsSuppress
-url=doc.css('cite')
 
+puts serps #outputing for debugging
+
+
+#go through the serps NodeList one by one and extract the href attribute of the anchor tags
+#TO DO: this needs to be turned into a method
+(0..serps.length-1).each {|i|
+  serps_url = serps[i]["href"]
+  #remove junk that Google appends before and after the end of the returned search result pages
+  #using a funky regular expression
+  split_array = serps_url.split(/(http.*?)(?=&)/)
+  puts split_array[1]
+}
+
+
+
+#format output header to the console
 puts ""
 puts "Search term: "
 puts ""
@@ -36,14 +53,16 @@ puts "# search results: " + serps.length.to_s
 puts ""
 puts "*************"
 
-jump=0
+jump=0  #this is currently a hack to deal with image results appearing in search listings - needs fixing
 
 for i in (0..serps.length-1)
-  if serps[i].text.include? "Images"
+  if serps[i].text.include? "Images" #same hack, dealing with pages that contain image result listings
     jump +=1
   else
+    #return the link titles from the listing on the page
     puts "Rank " + (i+1-jump).to_s + " Listing: " + serps[i].text
-    puts "Rank " + (i+1-jump).to_s + " URL: " + url[i-jump].text
+    #return the link href from the listing on the page - needs fixing
+    #puts "Rank " + (i+1-jump).to_s + " URL: " + url[i-jump].text
     puts "__________"
     puts ""
   end
